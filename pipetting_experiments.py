57b0a2f7643f3e5b0f830fd7d1bc3cf0aba6f4fd
hiroto22 <tochihirototo.nya.49@gmail.com>
2023-04-27 19:33:15 +0900

    :sparkles: 分注定量実験コード

--- pipetting_experiments.py
+++ pipetting_experiments.py
@@ -2,14 +2,58 @@ from opentrons import protocol_api
 
 metadata = {
     'protocolName': 'Test Protocol',
-    'apiLevel': '2.13'
+    'apiLevel': '2.11'
 }
 
+# 20ul*50
+# def run(protocol: protocol_api.ProtocolContext):
+#    rack = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 7)
+#    tiprack = protocol.load_labware('opentrons_96_tiprack_20ul', 10)
+
+#    pipette = protocol.load_instrument('p20_single_gen2', 'left', tip_racks=[tiprack])
+
+#    pipette.pick_up_tip()
+
+#    for _ in range(50):
+#         pipette.transfer(20, rack['A1'], rack['A2'], new_tip='never')
+        
+#    pipette.drop_tip()
+
+
+# 1ul*100
+# def run(protocol: protocol_api.ProtocolContext):
+#    rack = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 7)
+#    tiprack = protocol.load_labware('opentrons_96_tiprack_300ul', 10)
+
+#    pipette = protocol.load_instrument('p20_single_gen2', 'left', tip_racks=[tiprack])
+
+#    pipette.pick_up_tip(tiprack['A2'])
+
+#    for _ in range(100):
+#         pipette.transfer(1, rack['A1'], rack['A2'], new_tip='never')
+        
+#    pipette.drop_tip()
+
+# # 20ul*50
+# def run(protocol: protocol_api.ProtocolContext):
+#     rack = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 7)
+#     tiprack = protocol.load_labware('opentrons_96_tiprack_300ul', 11)
+#     pipette = protocol.load_instrument('p300_single_gen2', 'right', tip_racks=[tiprack])
+#     pipette.pick_up_tip()
+
+#     for _ in range(50):
+#         pipette.transfer(20, rack['A1'], rack['A2'], new_tip='never')
+        
+#     pipette.drop_tip()
+
+# 300ul*4
 def run(protocol: protocol_api.ProtocolContext):
-    plate = protocol.load_labware('corning_96_wellplate_360ul_flat', 2)
-    tiprack = protocol.load_labware('opentrons_96_tiprack_300ul', 1)
-    pipette = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack])
+    rack = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 7)
+    tiprack = protocol.load_labware('opentrons_96_tiprack_300ul', 11)
+    pipette = protocol.load_instrument('p300_single_gen2', 'right', tip_racks=[tiprack])
     pipette.pick_up_tip()
-    pipette.aspirate(100, plate['A1'])
-    pipette.dispense(100, plate['A2'])
+
+    for _ in range(4):
+        pipette.transfer(300, rack['A1'], rack['A2'], new_tip='never')
+        
     pipette.drop_tip()
